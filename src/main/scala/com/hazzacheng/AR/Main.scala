b9077457250929c28ad9b1d66d1bce558305b8d7
HazzaCheng <hazzacheng@gmail.com>
2017-09-26 22:19:31 +0800

    Initial commit

---
+++ src/main/scala/com/hazzacheng/AR/Main.scala
@@ -0,0 +1,14 @@
+package com.hazzacheng.AR
+
+/**
+  * Created with IntelliJ IDEA.
+  *
+  * Description: 
+  * User: HazzaCheng
+  * Contact: hazzacheng@gmail.com
+  * Date: 2017-09-26
+  * Time: 10:04 PM
+  */
+object Main {
+
+}
